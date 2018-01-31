59082f51e069c797b40f68ece87da7cd324207cd
DivXZero <silv3rlogic@gmail.com>
2018-01-31 10:44:46 -0500

    Update update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,10 +2,10 @@
 
 cd ../
 
-bundle install
-yarn install
+sudo bundle install
+sudo yarn install
 
-bundle exec rake db:create db:migrate
+sudo bundle exec rake db:create db:migrate
 
 sudo cp host.conf /etc/apache2/sites-available/000-default.conf
 
